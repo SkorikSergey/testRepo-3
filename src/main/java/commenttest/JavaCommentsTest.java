c46d20e728648dbcc4ef7c12671dbe49465ab398
SkorikSergey <useri8dg@some.mail>
2017-04-12 16:50:06 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1491929009113
+//first_change1492009969194
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
